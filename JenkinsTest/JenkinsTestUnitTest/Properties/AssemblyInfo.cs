c3e4a9443354671b2e97938883245a03100b0824
MinatoNamakaze <87276332+KurokoTetsuya64@users.noreply.github.com>
2021-10-26 10:40:13 +0400

    AddJenkinsTest

---
+++ JenkinsTest/JenkinsTestUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JenkinsTestUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JenkinsTestUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("60fbad0c-fd3d-42bc-aa72-8621cc0311ad")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ JenkinsTest/JenkinsTestUnitTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using JenkinsTest;
+
+namespace JenkinsTestUnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Animal animal = new Animal();
+            int expectedNumber = 5;
+            int number = animal.GetNumberOfLegs(expectedNumber);
+            Assert.AreEqual(expectedNumber, number);
+
+        }
+    }
+}
