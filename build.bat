712d21e888a1649c549fa17d396216cd28bff21b
MinatoNamakaze <87276332+KurokoTetsuya64@users.noreply.github.com>
2021-11-11 18:25:06 +0400

    Most important thinks
    :)

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-cd JenkinsTest\
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe Test.sln /p:Configuration=Release
+
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe JenkinsTest\JenkinsTest.sln
