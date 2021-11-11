af2fd4286d9d3faaa47bdf4c900c03d2922dd190
MinatoNamakaze <87276332+KurokoTetsuya64@users.noreply.github.com>
2021-11-11 19:25:54 +0400

    Fix tests
    add rep

--- tests.bat
+++ tests.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" "JenkinsTest\JenkinsTestUnitTest\bin\Debug\JenkinsTestUnitTest.dll"
+"E:\VISUAL STUD\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" "JenkinsTest\JenkinsTestUnitTest\bin\Debug\JenkinsTestUnitTest.dll"
